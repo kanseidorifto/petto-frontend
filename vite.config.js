f243c8e840a201c4cbb6dc577fbd34ffafb52d97
kanseidorifto <39811215+kanseidorifto@users.noreply.github.com>
2023-06-05 11:43:39 +0300

    env fix v2

--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,19 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-	const env = loadEnv(mode, process.cwd(), '');
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
 	return {
 		server: {
 			proxy: {
 				'/api': {
-					target: env.VITE_APP_API_URL,
+					target: process.VITE_APP_API_URL,
 					changeOrigin: true,
-					// secure: false,
+					secure: false,
 					ws: true,
 					rewrite: (path) => path.replace(/^\/api/, ''),
 				},
 			},
-		},
-		define: {
-			// 'process.env.YOUR_STRING_VARIABLE': JSON.stringify(env.YOUR_STRING_VARIABLE),
-			// 'process.env.YOUR_BOOLEAN_VARIABLE': env.YOUR_BOOLEAN_VARIABLE,
-			// If you want to exposes all env variables, which is not recommended
-			'process.env': env,
 		},
 		plugins: [react()],
 	};
