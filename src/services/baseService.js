fe87c39f4b329be93752fea5087e6a011cab4966
kanseidorifto <39811215+kanseidorifto@users.noreply.github.com>
2023-06-05 12:22:50 +0300

    fix cred

--- src/services/baseService.js
+++ src/services/baseService.js
@@ -7,7 +7,7 @@ export const baseApi = createApi({
 		// base url of backend API
 		baseUrl: backendURL,
 		mode: 'no-cors',
-		credentials: 'include',
+		credentials: 'omit',
 		// prepareHeaders is used to configure the header of every request and gives access to getState which we use to include the token from the store
 		prepareHeaders: (headers, { getState }) => {
 			const token = getState().auth.userToken;
